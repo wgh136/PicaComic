d19519ca65731185a66b6eac2d3c1b451d816143
Nyne <wgh1624044369@gmail.com>
2023-05-16 15:55:51 +0800

    优化ui

--- lib/views/eh_views/eh_login_page.dart
+++ lib/views/eh_views/eh_login_page.dart
@@ -24,117 +24,126 @@ class _EhLoginPageState extends State<EhLoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("登录E-Hentai账户"),),
-      body: Center(
-        child: SizedBox(
-          height: 450,
-          width: 400,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text("  使用cookie登录",style: TextStyle(fontSize: 18),),
-              const SizedBox(height: 3,),
-              Padding(
-                padding: const EdgeInsets.all(5),
-                child: TextField(
-                  controller: c1,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "ipb_member_id"
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(5),
-                child: TextField(
-                  controller: c2,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "ipb_pass_hash"
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(5),
-                child: TextField(
-                  controller: c3,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "igneous(非必要)"
-                  ),
-                ),
-              ),
-              Center(
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
-                  child: !logging?FilledButton(
-                    child: const Text("登录"),
-                    onPressed: (){
-                      if(c1.text=="" || c2.text==""){
-                        showMessage(context, "请填写完整");
-                      }else{
-                        login(c1.text, c2.text, c3.text);
-                      }
-                    },
-                  ):const CircularProgressIndicator(),
-                ),
-              ),
-              const SizedBox(height: 10,),
-              if(GetPlatform.isAndroid)
-              SizedBox(
-                width: 150,
-                height: 40,
-                child: TextButton(
-                  onPressed: ()async{
-                    var browser = LoginInBrowser(() async{
-                      CookieManager cookieManager = CookieManager.instance();
-                      var id = await cookieManager.getCookie(url: Uri.parse(".e-hentai.org"), name: "ipb_member_id");
-                      var hash = await cookieManager.getCookie(url: Uri.parse(".e-hentai.org"), name: "ipb_pass_hash");
-                      var igneous = await cookieManager.getCookie(url: Uri.parse(".exhentai.org"), name: "igneous");
-                      try {
-                        login(id!.value, hash!.value, igneous==null?"":igneous.value);
-                      }
-                      catch(e){
-                        showMessage(Get.context, "登录失败");
-                      }
-                    });
-                    await browser.openUrlRequest(urlRequest: URLRequest(url: Uri.parse("https://forums.e-hentai.org/index.php?act=Login&CODE=00")));
-                  },
-                  child: const Row(
-                    children: [
-                      Text("在Webview中登录"),
-                      Icon(Icons.arrow_outward,size: 15,)
-                    ],
-                  ),
-                ),
-              ),
-              if(GetPlatform.isAndroid)
-              const SizedBox(height: 5,),
-              Center(child: SizedBox(
-                width: 68,
-                height: 40,
-                child: TextButton(
-                  onPressed: ()=>launchUrlString("https://forums.e-hentai.org/index.php?act=Reg&CODE=00",mode: LaunchMode.externalApplication),
-                  child: const Row(
-                    children: [
-                      Text("注册"),
-                      Icon(Icons.arrow_outward,size: 15,)
-                    ],
-                  ),
-                ),
-              ),),
-              const SizedBox(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: SizedBox(
+                height: 400,
                 width: 400,
-                height: 40,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Icon(Icons.info_outline,size: 20,),
-                    Text("由于需要captcha响应, 暂不支持直接密码登录",maxLines: 2,)
+                    const Text("  使用cookie登录",style: TextStyle(fontSize: 18),),
+                    const SizedBox(height: 3,),
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: TextField(
+                        controller: c1,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "ipb_member_id"
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: TextField(
+                        controller: c2,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "ipb_pass_hash"
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: TextField(
+                        controller: c3,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "igneous(非必要)"
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                        child: !logging?FilledButton(
+                          child: const Text("登录"),
+                          onPressed: (){
+                            if(c1.text=="" || c2.text==""){
+                              showMessage(context, "请填写完整");
+                            }else{
+                              login(c1.text, c2.text, c3.text);
+                            }
+                          },
+                        ):const CircularProgressIndicator(),
+                      ),
+                    ),
+                    const SizedBox(height: 10,),
+                    if(GetPlatform.isAndroid)
+                      Center(
+                        child: SizedBox(
+                          width: 150,
+                          height: 40,
+                          child: TextButton(
+                            onPressed: ()async{
+                              var browser = LoginInBrowser(() async{
+                                CookieManager cookieManager = CookieManager.instance();
+                                var id = await cookieManager.getCookie(url: Uri.parse(".e-hentai.org"), name: "ipb_member_id");
+                                var hash = await cookieManager.getCookie(url: Uri.parse(".e-hentai.org"), name: "ipb_pass_hash");
+                                var igneous = await cookieManager.getCookie(url: Uri.parse(".exhentai.org"), name: "igneous");
+                                try {
+                                  login(id!.value, hash!.value, igneous==null?"":igneous.value);
+                                }
+                                catch(e){
+                                  showMessage(Get.context, "登录失败");
+                                }
+                              });
+                              await browser.openUrlRequest(urlRequest: URLRequest(url: Uri.parse("https://forums.e-hentai.org/index.php?act=Login&CODE=00")));
+                            },
+                            child: const Row(
+                              children: [
+                                Text("在Webview中登录"),
+                                Icon(Icons.arrow_outward,size: 15,)
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    if(GetPlatform.isAndroid)
+                      const SizedBox(height: 5,),
+                    Center(child: SizedBox(
+                      width: 68,
+                      height: 40,
+                      child: TextButton(
+                        onPressed: ()=>launchUrlString("https://forums.e-hentai.org/index.php?act=Reg&CODE=00",mode: LaunchMode.externalApplication),
+                        child: const Row(
+                          children: [
+                            Text("注册"),
+                            Icon(Icons.arrow_outward,size: 15,)
+                          ],
+                        ),
+                      ),
+                    ),),
+                    const SizedBox(
+                      width: 400,
+                      height: 40,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.info_outline,size: 20,),
+                          Text("由于需要captcha响应, 暂不支持直接密码登录",maxLines: 2,)
+                        ],
+                      ),
+                    )
                   ],
                 ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
       ),
     );
