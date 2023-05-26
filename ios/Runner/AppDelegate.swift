c8a0e5500707ce6d5b92159949c6ad711652921a
Nyne <wgh1624044369@gmail.com>
2023-05-26 17:36:01 +0800

    ios methodChannel

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,23 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let channel = FlutterMethodChannel(name: "kokoiro.xyz.pica_comic/proxy",
+                                                binaryMessenger: controller.binaryMessenger)
+    channel.setMethodCallHandler({
+    (call: FlutterMethodCall, result: FlutterResult) -> Void in
+        result("No Proxy")
+    })
+    FlutterMethodChannel(name: "com.kokoiro.xyz.pica_comic/keepScreenOn",
+                                                    binaryMessenger: controller.binaryMessenger).setMethodCallHandler({
+       (call: FlutterMethodCall, result: FlutterResult) -> Void in
+          if call.method == "set"{
+            UIApplication.shared.isIdleTimerDisabled = true
+          }else{
+            UIApplication.shared.isIdleTimerDisabled = false
+          }
+       })
+    })
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
