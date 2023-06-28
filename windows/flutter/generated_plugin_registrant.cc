d5affdc6cc555cbbda73494a0acbe17584cb43a9
Nyne <wgh1624044369@gmail.com>
2023-06-28 22:32:37 +0800

    webview

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -8,6 +8,7 @@
 
 #include <dynamic_color/dynamic_color_plugin_c_api.h>
 #include <file_selector_windows/file_selector_windows.h>
+#include <flutter_windows_webview/flutter_windows_webview_plugin_c_api.h>
 #include <local_auth_windows/local_auth_plugin.h>
 #include <share_plus/share_plus_windows_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
@@ -17,6 +18,8 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("DynamicColorPluginCApi"));
   FileSelectorWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSelectorWindows"));
+  FlutterWindowsWebviewPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterWindowsWebviewPluginCApi"));
   LocalAuthPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("LocalAuthPlugin"));
   SharePlusWindowsPluginCApiRegisterWithRegistrar(
