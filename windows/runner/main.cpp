7fc3706d4e168f7621b6c7edc424bf2dc20cd6f6
wgh136 <wgh1624044369@gmail.com>
2023-07-14 20:58:30 +0800

    调整windows端初始窗口大小

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -25,7 +25,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
 
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
-  Win32Window::Size size(1280, 720);
+  Win32Window::Size size(1280, 800);
   if (!window.Create(L"Pica Comic", origin, size)) {
     return EXIT_FAILURE;
   }
