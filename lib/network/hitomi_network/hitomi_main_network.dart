ab6fa9fdce5c3afa90f478a3895f5f926e1f3a29
wgh136 <wgh1624044369@gmail.com>
2023-08-13 07:23:06 +0800

    修复hitomi网络请求问题

--- lib/network/hitomi_network/hitomi_main_network.dart
+++ lib/network/hitomi_network/hitomi_main_network.dart
@@ -30,6 +30,7 @@ class HiNetwork{
           sendTimeout: const Duration(seconds: 5),
           headers: {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+            "Referer": "https://hitomi.la/"
           }
       );
       var dio = CachedNetwork();
