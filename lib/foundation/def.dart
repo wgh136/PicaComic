5eec7f4df374cd0ff45e2b424f82048e1c438523
wgh19 <nyne19710@proton.me>
2024-03-19 14:02:15 +0800

    update version code

--- lib/foundation/def.dart
+++ lib/foundation/def.dart
@@ -18,7 +18,7 @@ const String webUA =
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36";
 
 //App版本
-const appVersion = "3.0.1-dev";
+const appVersion = "3.0.1";
 
 //定义宽屏设备的临界值
 const changePoint = 600;
