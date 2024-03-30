c619ffc597524fa838524012aa8f1dc0dff7d4d5
wgh19 <nyne19710@proton.me>
2024-03-30 13:50:28 +0800

    improve performance

--- lib/foundation/image_loader/image_recombine.dart
+++ lib/foundation/image_loader/image_recombine.dart
@@ -1,11 +1,13 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'dart:isolate';
 import 'package:flutter/foundation.dart';
 import 'package:image/image.dart' as image;
 import 'package:crypto/crypto.dart';
 
 /// 转换自 https://github.com/tonquer/JMComic-qt/blob/main/src/tools/tool.py
-int getSegmentationNum(String epsId, String scrambleID, String pictureName) {
+int _getSegmentationNum(String epsId, String scrambleID, String pictureName) {
   int scrambleId = int.parse(scrambleID);
   int epsID = int.parse(epsId);
   int num = 0;
@@ -34,8 +36,8 @@ int getSegmentationNum(String epsId, String scrambleID, String pictureName) {
 }
 
 /// 转换自 https://github.com/tonquer/JMComic-qt/blob/main/src/tools/tool.py
-Future<Uint8List> segmentationPicture(RecombinationData data) async {
-  int num = getSegmentationNum(data.epsId, data.scrambleId, data.bookId);
+Future<Uint8List> _segmentationPicture(_RecombinationTask data) async {
+  int num = _getSegmentationNum(data.epsId, data.scrambleId, data.bookId);
 
   if (num <= 1) {
     return data.imgData;
@@ -79,8 +81,8 @@ Future<Uint8List> segmentationPicture(RecombinationData data) async {
   return image.encodeJpg(desImg);
 }
 
-Future<Uint8List> recombineImageAndWriteFile(RecombinationData data) async {
-  var bytes = await segmentationPicture(data);
+Future<Uint8List> _recombineImageAndWriteFile(_RecombinationTask data) async {
+  var bytes = await _segmentationPicture(data);
   var file = File(data.savePath!);
   if (file.existsSync()) {
     file.deleteSync();
@@ -90,36 +92,105 @@ Future<Uint8List> recombineImageAndWriteFile(RecombinationData data) async {
 }
 
 
-class RecombinationData {
+class _RecombinationTask {
   Uint8List imgData;
   String epsId;
   String scrambleId;
   String bookId;
   String? savePath;
+  Completer<Uint8List>? completer;
 
-  RecombinationData(this.imgData, this.epsId, this.scrambleId, this.bookId,
+  _RecombinationTask removeCompleter(){
+    return _RecombinationTask(imgData, epsId, scrambleId, bookId, null, savePath);
+  }
+
+  _RecombinationTask(this.imgData, this.epsId, this.scrambleId, this.bookId, this.completer,
       [this.savePath]);
 }
 
-int loadingItems = 0;
+class JmRecombine{
+  static Isolate? _isolate;
 
-final maxLoadingItems = Platform.isAndroid || Platform.isIOS ? 3 : 5;
+  static ReceivePort? _receivePort;
+
+  static SendPort? _sendPort;
+
+  static final List<_RecombinationTask> _tasks = [];
+
+  static _RecombinationTask? _current;
+
+  static Timer? _timer;
+
+  static Future<Uint8List> recombineImage(Uint8List imgData, String epsId,
+      String scrambleId, String bookId, String savePath) async{
+    _timer?.cancel();
+    _timer = null;
+    Completer<Uint8List> completer = Completer();
+    _RecombinationTask task =
+      _RecombinationTask(imgData, epsId, scrambleId, bookId, completer, savePath);
+    _tasks.add(task);
+    if(_isolate == null){
+      await _start();
+    }
+    _pushTask();
+    return completer.future;
+  }
+
+  static void _pushTask(){
+    if(_tasks.isEmpty){
+      _timer ??= Timer(const Duration(seconds: 5), (){
+          if(_tasks.isEmpty){
+            _isolate!.kill(priority: Isolate.immediate);
+            _isolate = null;
+            _receivePort?.close();
+            _receivePort = null;
+            _sendPort = null;
+            _timer = null;
+          }
+        });
+      return;
+    }
+
+    if(_sendPort != null && _current == null){
+      _current = _tasks.removeAt(0);
+      _sendPort!.send(_current!.removeCompleter());
+    }
+  }
+
+  static _start() async{
+    _receivePort = ReceivePort();
+    _isolate = await Isolate.spawn(_run, _receivePort!.sendPort);
+    _listen();
+    await Future.delayed(const Duration(milliseconds: 10));
+  }
+
+  static void _listen(){
+    _receivePort!.listen((message) {
+      if (message is SendPort){
+        _sendPort = message;
+      } else if(message is Uint8List){
+        _current!.completer!.complete(message);
+        _current = null;
+        _pushTask();
+      }
+    });
+  }
+
+  static void _run(SendPort port){
+    _receivePort = ReceivePort();
+    port.send(_receivePort!.sendPort);
+    _receivePort!.listen((message) async{
+      if (message is _RecombinationTask){
+        _RecombinationTask task = message;
+        Uint8List bytes = await _recombineImageAndWriteFile(task);
+        port.send(bytes);
+      }
+    });
+  }
+}
 
 ///启动一个新的线程转换图片并且写入文件
 Future<Uint8List> startRecombineAndWriteImage(Uint8List imgData, String epsId,
-    String scrambleId, String bookId, String savePath) async {
-  while(loadingItems >= maxLoadingItems){
-    await Future.delayed(const Duration(milliseconds: 100));
-  }
-  loadingItems++;
-  try {
-    return await compute(recombineImageAndWriteFile,
-        RecombinationData(imgData, epsId, scrambleId, bookId, savePath));
-  }
-  catch(e){
-    rethrow;
-  }
-  finally{
-    loadingItems--;
-  }
+    String scrambleId, String bookId, String savePath) {
+  return JmRecombine.recombineImage(imgData, epsId, scrambleId, bookId, savePath);
 }
