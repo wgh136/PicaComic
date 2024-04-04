858b20d49faf4c8dc9a9d0fa787c126f2d9a2ba0
wgh19 <nyne19710@proton.me>
2024-04-04 16:50:29 +0800

    update windows build

--- build_windows.py
+++ build_windows.py
@@ -19,7 +19,8 @@ subprocess.run(["flutter", "build", "windows"], shell=True)
 file = open('pubspec.yaml', 'w')
 file.write(content)
 
-os.remove("build/app-windows.zip")
+if os.path.exists("build/app-windows.zip"):
+    os.remove("build/app-windows.zip")
 
 # 压缩build/windows/x64/runner/Release, 生成app-windows.zip, 使用tar命令
 subprocess.run(["tar", "-a", "-c", "-f", "build/app-windows.zip", "-C", "build/windows/x64/runner/Release", "."]
