30ba3533a5f8476e7205c9b9ccde47f132d8053e
wgh19 <nyne19710@proton.me>
2024-08-24 20:43:13 +0800

    fix network cache

--- lib/network/cache_network.dart
+++ lib/network/cache_network.dart
@@ -12,23 +12,24 @@ import 'app_dio.dart';
 class CachedNetwork {
   Future<CachedNetworkRes<String>> get(String url, BaseOptions options,
       {CacheExpiredTime expiredTime = CacheExpiredTime.short,
-      CookieJarSql? cookieJar, bool log = true, bool http2 = false}) async {
+      CookieJarSql? cookieJar,
+      bool log = true,
+      bool http2 = false}) async {
     await setNetworkProxy();
-    var fileName = md5
-        .convert(const Utf8Encoder()
-            .convert(url.replaceFirst("inline_set=ts_l&", "")))
-        .toString();
+    var fileName = md5.convert(const Utf8Encoder().convert(url)).toString();
     if (fileName.length > 20) {
       fileName = fileName.substring(0, 21);
     }
     final key = url;
-    var cache = await CacheManager().findCache(key);
-    if (cache != null) {
-      var file = File(cache);
-      return CachedNetworkRes(await file.readAsString(), 200, url);
+    if (expiredTime != CacheExpiredTime.no) {
+      var cache = await CacheManager().findCache(key);
+      if (cache != null) {
+        var file = File(cache);
+        return CachedNetworkRes(await file.readAsString(), 200, url);
+      }
     }
     options.responseType = ResponseType.bytes;
-    var dio = log?logDio(options, http2):Dio(options);
+    var dio = log ? logDio(options, http2) : Dio(options);
     if (cookieJar != null) {
       dio.interceptors.add(CookieManagerSql(cookieJar));
     }
@@ -40,11 +41,11 @@ class CachedNetwork {
     if (expiredTime != CacheExpiredTime.no) {
       await CacheManager().writeCache(key, res.data!, expiredTime.time);
     }
-    return CachedNetworkRes(utf8.decode(res.data!, allowMalformed: true), res.statusCode,
-        res.realUri.toString(), res.headers.map);
+    return CachedNetworkRes(utf8.decode(res.data!, allowMalformed: true),
+        res.statusCode, res.realUri.toString(), res.headers.map);
   }
 
-  void delete(String url) async{
+  void delete(String url) async {
     await CacheManager().delete(url);
   }
 }
@@ -67,5 +68,6 @@ class CachedNetworkRes<T> {
   Map<String, List<String>> headers;
   String url;
 
-  CachedNetworkRes(this.data, this.statusCode, this.url, [this.headers = const {}]);
+  CachedNetworkRes(this.data, this.statusCode, this.url,
+      [this.headers = const {}]);
 }
