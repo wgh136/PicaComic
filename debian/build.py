2b26465cdfdbbf89baa76307a8617628272f871e
nyne <nyne19710@proton.me>
2024-08-28 15:03:54 +0800

    debian package build script

---
+++ debian/build.py
@@ -0,0 +1,26 @@
+import subprocess
+
+debianContent = ''
+desktopContent = ''
+version = ''
+
+with open('debian/debian.yaml', 'r') as f:
+    debianContent = f.read()
+with open('debian/gui/pica.desktop', 'r') as f:
+    desktopContent = f.read()
+with open('pubspec.yaml', 'r') as f:
+    version = str.split(str.split(f.read(), 'version: ')[1], '+')[0]
+
+with open('debian/debian.yaml', 'w') as f:
+    f.write(debianContent.replace('{{Version}}', version))
+with open('debian/gui/pixes.desktop', 'w') as f:
+    f.write(desktopContent.replace('{{Version}}', version))
+
+subprocess.run(["flutter", "build", "linux"])
+
+subprocess.run(["$HOME/.pub-cache/bin/flutter_to_debian"], shell=True)
+
+with open('debian/debian.yaml', 'w') as f:
+    f.write(debianContent)
+with open('debian/gui/pixes.desktop', 'w') as f:
+    f.write(desktopContent)
